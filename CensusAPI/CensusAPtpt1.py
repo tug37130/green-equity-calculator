1aa534bede804a73d23591132538601c2e86ba83
tug37130 <149119106+tug37130@users.noreply.github.com>
2024-02-23 19:10:50 -0500

    Add files via upload

---
+++ CensusAPI/CensusAPtpt1.py
@@ -0,0 +1,26 @@
+# Base URL segments
+host = 'https://api.census.gov/data'
+year = '/2019'
+dataset_acronym = '/acs/acs5/profile'
+g = '?get='
+# Location argument will need to relate to other application arguments
+location = '&for=us:*'
+
+# List of variables to pull
+variables = [
+    'DP02,DP02_0011PE,DP02_0068PE',
+    'DP03,DP03_0119PE',
+    'DP04,DP04_0046PE'
+]
+
+# API key - replace 'your_api_key' with your actual API key
+usr_key = f#place key here in quotations)
+
+# Loop through variables and make API requests
+for i, variable_set in enumerate(variables, start=1):
+    # Construct the API request URL for each variable set
+    query_url = f"{host}{year}{dataset_acronym}{g}{variable_set}{location}&key={usr_key}"
+    # Use request package to call out to the API
+    response = requests.get(query_url)
+    print(response.text)
+
