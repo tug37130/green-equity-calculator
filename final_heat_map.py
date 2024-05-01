e4fc1235c83ebacf47b8c0709246f519dc2dbd88
venkatasatishjasti <118797996+venkatasatishjasti@users.noreply.github.com>
2024-05-01 00:27:50 -0400

    Update final_heat_map.py

--- final_heat_map.py
+++ final_heat_map.py
@@ -59,17 +59,21 @@ def find_least_cloudy_item(catalog_url, bbox, time_range, collections):
         return None
 
 
-
+# Function to load band data from a selected item using Open Data Cube (ODC)
 def load_band_data(selected_item, bands_of_interest, bbox_of_interest):
+    # Load data from the selected item with specified bands and bounding box
     data = odc.stac.stac_load(
         [selected_item], bands=bands_of_interest, bbox=bbox_of_interest
     ).isel(time=0)
     return data
-
+    
+# Function to get information about a specific band from a selected item
 def get_band_info(selected_item, band_name):
+    # Extract information about the specified band from the selected item
     band_info = selected_item.assets[band_name].extra_fields["raster:bands"][0]
     return band_info
 
+# Function to convert temperature data based on band information
 def convert_temperature(data, band_info):
     temperature = data.astype(float)
     temperature *= band_info["scale"]
@@ -77,11 +81,11 @@ def convert_temperature(data, band_info):
     celsius_temp = temperature - 273.15
     return celsius_temp
 
-# downloading and saving the raster layer
+# Function for saving a temperature raster layer to a GeoTIFF file
 def save_temperature_raster(temp_raster_file, data_array, bbox, crs_epsg=5070):
     transform = from_bounds(*bbox, width=data_array.shape[1], height=data_array.shape[0])
     crs = rasterio.crs.CRS.from_epsg(crs_epsg)
-
+    # Open a new GeoTIFF file for writing
     with rasterio.open(temp_raster_file, 'w', driver='GTiff',
                        width=data_array.shape[1], height=data_array.shape[0],
                        count=1, dtype=data_array.dtype,
@@ -90,52 +94,54 @@ def save_temperature_raster(temp_raster_file, data_array, bbox, crs_epsg=5070):
 
 # Clip and create a mask layer from raster layer
 def clip_create_mask_layer(temp_raster_file, geodataframe, clipped_masked_raster):
+    # Extract geometries from the GeoDataFrame
     shapes = [geom for geom in geodataframe.geometry]
-
+    
+     # Open the temp raster dataset
     with rasterio.open(temp_raster_file) as src:
+        # Perform masking operation using geometries
         out_image, out_transform = mask(src, shapes, crop=True)
+        # Get metadata of the input raster
         out_meta = src.meta
-
+    # Update metadata for the output raster
     out_meta.update({"driver": "GTiff",
                      "height": out_image.shape[1],
                      "width": out_image.shape[2],
                      "compress": 'lzw',
                      "transform": out_transform})
-
+    # Create a new raster file with masked data
     with rasterio.open(clipped_masked_raster, "w", **out_meta) as dest:
         dest.write(out_image)
 
 # Extracting temperature data from cliped raster layer iterating through each polygon of census tracts
 
-def extract_temp_write_shapefile(inraster, final_gdf, output_shp_res):
+def extract_temp_add_column(inraster, final_gdf):
     # Open the raster dataset
     temp_dataset = rasterio.open(inraster)
 
-    # Prepare the polygon shapefile and then do the overlay of the raster data and the vector data
-    schema = {
-        'geometry': 'Polygon',
-        'properties': {'temp': 'float'}
-    }
+    # Initialize an empty list to store temperature values
+    temp_values = []
 
-    with fiona.open(output_shp_res, 'w', driver="ESRI Shapefile", crs=final_gdf.crs, schema=schema) as output:
-        for idx, row in final_gdf.iterrows():
-            geom = row['geometry']
-            shape = [geom]  # rasterio.mask.mask needs the shape to be in a list
+    # Iterate over each row in the GeoDataFrame
+    for idx, row in final_gdf.iterrows():
+        geom = row['geometry']
+        shape = [geom]  # rasterio.mask.mask needs the shape to be in a list
 
-            # Mask the raster using the polygon
-            outtemp_image, out_transform = mask(temp_dataset, shape, crop=True)
+        # Mask the raster using the polygon
+        outtemp_image, out_transform = mask(temp_dataset, shape, crop=True)
 
-            # Convert to Celsius using given information
-            temperature = outtemp_image.astype(float)
-            # Calculate the mean temperature value
-            mean_val = float(np.mean(temperature))
+        # Convert to Celsius using given information
+        temperature = outtemp_image.astype(float)
+        # Calculate the mean temperature value
+        mean_val = float(np.mean(temperature))
 
-            # Update the calculated temperature value to the field
-            properties = {'temp': mean_val}
+        # Append the mean temperature value to the list
+        temp_values.append(mean_val)
 
-            output.write({'geometry': geom,
-                          'properties': properties
-                          })
+    # Add a new column 'temp' to the GeoDataFrame and populate it with the temperature values
+    final_gdf['temp'] = temp_values
+
+    return final_gdf
 
 
 # usage
@@ -149,47 +155,30 @@ def write_attach_temp(final_gdf, output_folder):
     # get bbox
     bbox_of_interest = set_bbox(final_gdf)
     
-    # static variables
+    # Connect to the STAC catalog and search for items within the time and area of interest
     catalog_url = "https://planetarycomputer.microsoft.com/api/stac/v1"
     collections_of_interest = ["landsat-c2-l2"]
     time_of_interest = "2021-01-01/2021-12-31"
-    
-    #
-    bands_of_interest = ["lwir11"]
-    
-    # writing raster
-    output_folder = output_folder
-    temp_raster_file = "temperature.tif"
-    crs_epsg = 5070
-    
-    # 
     selected_item = find_least_cloudy_item(catalog_url, bbox_of_interest, time_of_interest, collections_of_interest)
-    if selected_item:
-        print(
-        f"Choosing {selected_item.id} from {selected_item.datetime.date()}"
-        + f" with {selected_item.properties.get('eo:cloud_cover', 'N/A')}% cloud cover"
-        )
-    
     # Load and process temperature data
     data = load_band_data(selected_item, bands_of_interest, bbox_of_interest)
     band_info = get_band_info(selected_item, bands_of_interest)
     celsius_data = convert_temperature(data["lwir11"], band_info)
-    
+    # writing raster
+    output_folder = output_folder
+    crs_epsg = 5070
+    # Save temperature raster as a GeoTIFF
+    temp_raster_file = "temperature.tif"
     save_temperature_raster(temp_raster_file, celsius_data, bbox_of_interest, crs_epsg)
     # Clip temperature raster to the gdf
     clipped_masked_raster = 'mask.tif'
-    
     clip_create_mask_layer(temp_raster_file, final_gdf, clipped_masked_raster)
-    
-    
-    # Extract temperature data for each Census tract in final_gdf
-    output_shp_res = 'temperature_Census-tract.shp'
-    extract_temp_write_shapefile(clipped_masked_raster, final_gdf, output_shp_res)
-
+    # Extract temperature data for each Census tract using final_gdf
+    extract_temp_add_column(clipped_masked_raster, final_gdf)
+    final_gdf = extract_temp_add_column(clipped_masked_raster, final_gdf)
+    #output path for raster files
     output_path1 = os.path.join(output_folder,temp_raster_file)
-    output_path2 = os.path.join(output_folder,final_gdf)
-    output_path3 = os.path.join(output_folder,clipped_masked_raster)
-    output_path4 = os.path.join(output_folder,output_shp_res)
+    output_path2 = os.path.join(output_folder,clipped_masked_raster)
 
 
 
