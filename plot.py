a38154542c5678ac86871b8f5207aaf98f91e98f
tug37130 <149119106+tug37130@users.noreply.github.com>
2024-05-02 12:08:18 -0400

    Plotting and writing
    Split up the script into two separate scripts/functino.

---
+++ plot.py
@@ -0,0 +1,104 @@
+'''
+This script takes the finalized geodataframe and plots it based on the equity score field.
+Returns a plotted geodataframe pop-up.
+Note this will need to be updated with finalized variables and thresholds.
+'''
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.colors import Normalize
+from matplotlib import cm
+import mapclassify as mc
+# This import doesn't seem to work, but the following does
+# from matplotlib_scalebar.scalebar import ScaleBar
+from matplotlib import ScaleBar
+
+def plot_gdf(gdf):
+    # create a empty plot for the choropleth map
+    fig, ax = plt.subplots(1, figsize=(8, 8))
+
+    # the number of categories
+    n_class = 4
+
+    # Set the color scheme
+    cmap = plt.cm.get_cmap('YlGnBu', n_class)
+
+    # This is the field which the choropleth map will be visualized
+    field = 'FINAL_COLUMN_NAME'
+
+    # Plot the choropleth map based on the field, this can be changed as needed ('equal_interval', 'quantiles', 'FisherJenks', 'NaturalBreaks')
+    # NaturalBreaks probably works best
+    gdf.plot(column=field, 
+                        cmap='YlGnBu', 
+                        edgecolor='0.5', 
+                        ax = ax,
+                        linewidth=0.5,
+    #                      legend=True, #don't use the legend
+                        k=n_class, #the number of classes
+                        scheme='NaturalBreaks')
+
+
+    # Getting the Natural Breaks bins
+    nb = mc.NaturalBreaks(gdf[field].dropna(), k=n_class) # NaturalBreaks, FisherJenks, quantile
+    vals = list(nb.bins) # Get interval labels
+    vals.insert(0, gdf[field].min())
+
+
+    # Set the location of the legend (x0, y0, width, height), can be adjusted as needed
+    axins = ax.inset_axes([0.2, 1., 0.6, 0.03])
+
+    # Create a color bar for the map
+    norm = mpl.colors.BoundaryNorm(vals, cmap.N)
+    n_cmap = cm.ScalarMappable(norm=norm, cmap=cmap)
+    cbar = ax.get_figure().colorbar(n_cmap, 
+                                    orientation='horizontal', 
+                                    cax=axins,
+                                    spacing='proportional',  # Distance in legend proportional to the value
+                                    fraction=0.046, 
+                                    pad=0.0, 
+                                    ticks=vals,
+                                    shrink=0.4)
+
+    cbar.ax.set_xlabel('GREEN EQUITY SCORE')
+
+    # Color
+    scale2 = ScaleBar(
+        dx=1, 
+    #     label='Scale 2', 
+        location='lower right',
+        font_properties={'family':'serif', 'size': 'large'},
+    #     color='#b32400', ##b32400
+    #     box_color='yellow',
+        box_alpha=1  # Transparecy, can change as needed
+    )
+
+    ax.add_artist(scale2)
+
+    ax.set_axis_off()
+    plt.axis('equal')
+    final_plot = plt.show()
+
+    return final_plot
+
+
+
+
+
+
+
+
+
+    # Plot the choropleth map
+    gdf.plot(column='COLUMN NAME')
+    
+    # Add a title and legend
+    plt.title('Green Equity Score')
+    plt.legend()
+
+    # Show the plot
+    final_plot = plt.show()
+    
+    return final_plot
+
+
--- write_shp.py
+++ write_shp.py
@@ -5,25 +5,6 @@ Created on Tue Apr 30 19:17:30 2024
 @author: tug03166
 """
 
-def plot_gdf(gdf):
-
-    # Prepare the data
-    # ...
-    
-    # Plot the choropleth map
-    gdf.plot(column='green_equity_index_score')
-    
-    # Add a title and legend
-    plt.title('Choropleth Map')
-    plt.legend()
-
-    # Show the plot
-    final_plot = plt.show()
-    
-    return final_plot
-
-
-
 # Writting final_gdf to harddrive as .shp
 import os
 def write_gdf_to_shp(gdf, output_folder=None):
